c612ac7e5b165860ef7d7f0b62322b731ca7a047
hkmgosu <erick.quiroz@live.com>
2022-02-26 08:40:44 -0300

    alpha

---
+++ .cache/sync-requires.js
@@ -0,0 +1,15 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\SP Festigame\\Documents\\work\\AetherApps\\gatsby-starter-hyperspace\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\SP Festigame\\Documents\\work\\AetherApps\\gatsby-starter-hyperspace\\src\\pages\\404.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("C:\\Users\\SP Festigame\\Documents\\work\\AetherApps\\gatsby-starter-hyperspace\\src\\pages\\about.js"))),
+  "component---src-pages-elements-js": hot(preferDefault(require("C:\\Users\\SP Festigame\\Documents\\work\\AetherApps\\gatsby-starter-hyperspace\\src\\pages\\elements.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\SP Festigame\\Documents\\work\\AetherApps\\gatsby-starter-hyperspace\\src\\pages\\index.js"))),
+  "component---src-pages-newentools-js": hot(preferDefault(require("C:\\Users\\SP Festigame\\Documents\\work\\AetherApps\\gatsby-starter-hyperspace\\src\\pages\\newentools.js")))
+}
+
