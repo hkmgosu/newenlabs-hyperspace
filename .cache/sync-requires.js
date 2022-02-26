64548b759cd4842971420b0b3abe3ab6866bc2f2
hkmgosu <erick.quiroz@live.com>
2022-02-26 12:42:06 -0300

    update to latest

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-caches-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("C:\\Users\\SP Festigame\\Documents\\work\\NewenLabs\\web-app\\newenlabs-hyperspace\\.cache\\caches\\gatsby-plugin-offline\\app-shell.js"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\SP Festigame\\Documents\\work\\NewenLabs\\web-app\\newenlabs-hyperspace\\.cache\\dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\SP Festigame\\Documents\\work\\NewenLabs\\web-app\\newenlabs-hyperspace\\src\\pages\\404.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("C:\\Users\\SP Festigame\\Documents\\work\\NewenLabs\\web-app\\newenlabs-hyperspace\\src\\pages\\about.js"))),
   "component---src-pages-elements-js": hot(preferDefault(require("C:\\Users\\SP Festigame\\Documents\\work\\NewenLabs\\web-app\\newenlabs-hyperspace\\src\\pages\\elements.js"))),
