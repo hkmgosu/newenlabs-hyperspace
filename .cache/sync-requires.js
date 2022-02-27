111a0b9e4ad66da18ad6d7e9a637e47bb63ce57a
Aukanaw <aukanaw@newenlabs.dev>
2022-02-27 03:19:49 -0300

    updated fixing details

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -1,14 +1,13 @@
-const { hot } = require("react-hot-loader/root")
 
 // prefer default export if available
 const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\SP Festigame\\Documents\\work\\NewenLabs\\web-app\\newenlabs-hyperspace\\.cache\\dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\SP Festigame\\Documents\\work\\NewenLabs\\web-app\\newenlabs-hyperspace\\src\\pages\\404.js"))),
-  "component---src-pages-about-js": hot(preferDefault(require("C:\\Users\\SP Festigame\\Documents\\work\\NewenLabs\\web-app\\newenlabs-hyperspace\\src\\pages\\about.js"))),
-  "component---src-pages-elements-js": hot(preferDefault(require("C:\\Users\\SP Festigame\\Documents\\work\\NewenLabs\\web-app\\newenlabs-hyperspace\\src\\pages\\elements.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\SP Festigame\\Documents\\work\\NewenLabs\\web-app\\newenlabs-hyperspace\\src\\pages\\index.js")))
+  "component---cache-dev-404-page-js": preferDefault(require("/home/usuario/Documentos/work/newenlabs-hyperspace/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/home/usuario/Documentos/work/newenlabs-hyperspace/src/pages/404.js")),
+  "component---src-pages-about-js": preferDefault(require("/home/usuario/Documentos/work/newenlabs-hyperspace/src/pages/about.js")),
+  "component---src-pages-elements-js": preferDefault(require("/home/usuario/Documentos/work/newenlabs-hyperspace/src/pages/elements.js")),
+  "component---src-pages-index-js": preferDefault(require("/home/usuario/Documentos/work/newenlabs-hyperspace/src/pages/index.js"))
 }
 
